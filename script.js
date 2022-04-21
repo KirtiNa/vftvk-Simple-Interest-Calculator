a7b93bb704ebb8409ffba6d8e4beb4d084caf6b4
Kirti Nawale <Kirti@li-7697afcc-3429-11b2-a85c-9090d60f0cb1.ibm.com>
2022-04-21 13:35:52 +0530

    Changed files for simple interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+   
+   
+    document.getElementById("result").innerHTML=" <br/> <br/>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+" </mark>%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark> "+year+"</mark>\<br\>"
+
 }
         
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validateForm()
+{
+    var x =  document.getElementById("principal").value;
+    if (x <= 0){
+    alert("Enter Positive Number")
+    document.getElementById("principal").focus()
+    }
+}
